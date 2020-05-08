45df9ec8a6f21d1ed560131c5919da0643be0913
Tony Freeman <tonyfreeman@users.noreply.github.com>
2020-05-08 22:27:45 +0300

    Simplify the API

--- Sources/Test/Shims.swift
+++ Sources/Test/Shims.swift
@@ -2,7 +2,7 @@ import XCTest
 
 public typealias TestCase = XCTestCase
 
-public func assert(
+public func expect(
     _ expression: @autoclosure () throws -> Bool,
     _ message: @autoclosure () -> String = "",
     file: StaticString = #file,
@@ -15,208 +15,30 @@ public func assert(
         line: line)
 }
 
-public func assertNil(
-    _ expression: @autoclosure () throws -> Any?,
-    _ message: @autoclosure () -> String = "",
-    file: StaticString = #file,
-    line: UInt = #line)
-{
-    XCTAssertNil(
-        try expression(),
-        message(),
-        file: file,
-        line: line)
-}
-
-public func assertNotNil(
-    _ expression: @autoclosure () throws -> Any?,
-    _ message: @autoclosure () -> String = "",
-    file: StaticString = #file,
-    line: UInt = #line)
-{
-    XCTAssertNotNil(
-        try expression(),
-        message(),
-        file: file,
-        line: line)
-}
-
-public func assertTrue(
-    _ expression: @autoclosure () throws -> Bool,
-    _ message: @autoclosure () -> String = "",
-    file: StaticString = #file,
-    line: UInt = #line)
-{
-    XCTAssertTrue(
-        try expression(),
-        message(),
-        file: file,
-        line: line)
-}
-
-public func assertFalse(
-    _ expression: @autoclosure () throws -> Bool,
-    _ message: @autoclosure () -> String = "",
-    file: StaticString = #file,
-    line: UInt = #line)
-{
-    XCTAssertFalse(
-        try expression(),
-        message(),
-        file: file,
-        line: line)
-}
-
-public func assertEqual<T>(
-    _ expression1: @autoclosure () throws -> T,
-    _ expression2: @autoclosure () throws -> T,
-    _ message: @autoclosure () -> String = "",
-    file: StaticString = #file,
-    line: UInt = #line) where T : Equatable
-{
-    XCTAssertEqual(
-        try expression1(),
-        try expression2(),
-        message(),
-        file: file,
-        line: line)
-}
-
-public func assertEqual<T>(
-    _ expression1: @autoclosure () throws -> T,
-    _ expression2: @autoclosure () throws -> T,
-    accuracy: T,
-    _ message: @autoclosure () -> String = "",
-    file: StaticString = #file,
-    line: UInt = #line) where T : FloatingPoint
-{
-    XCTAssertEqual(
-        try expression1(),
-        try expression2(),
-        accuracy: accuracy,
-        message(),
-        file: file,
-        line: line)
-}
-
-public func assertGreaterThan<T>(
-    _ expression1: @autoclosure () throws -> T,
-    _ expression2: @autoclosure () throws -> T,
-    _ message: @autoclosure () -> String = "",
-    file: StaticString = #file,
-    line: UInt = #line) where T : Comparable
-{
-    XCTAssertGreaterThan(
-        try expression1(),
-        try expression2(),
-        message(),
-        file: file,
-        line: line)
-}
-
-public func assertGreaterThanOrEqual<T>(
-    _ expression1: @autoclosure () throws -> T,
-    _ expression2: @autoclosure () throws -> T,
-    _ message: @autoclosure () -> String = "",
-    file: StaticString = #file,
-    line: UInt = #line) where T : Comparable
-{
-    XCTAssertGreaterThanOrEqual(
-        try expression1(),
-        try expression2(),
-        message(),
-        file: file,
-        line: line)
-}
-
-public func assertLessThan<T>(
-    _ expression1: @autoclosure () throws -> T,
-    _ expression2: @autoclosure () throws -> T,
-    _ message: @autoclosure () -> String = "",
-    file: StaticString = #file,
-    line: UInt = #line) where T : Comparable
-{
-    XCTAssertLessThan(
-        try expression1(),
-        try expression2(),
-        message(),
-        file: file,
-        line: line)
-}
-
-public func assertLessThanOrEqual<T>(
-    _ expression1: @autoclosure () throws -> T,
-    _ expression2: @autoclosure () throws -> T,
-    _ message: @autoclosure () -> String = "",
-    file: StaticString = #file,
-    line: UInt = #line) where T : Comparable
-{
-    XCTAssertLessThanOrEqual(
-        try expression1(),
-        try expression2(),
-        message(),
-        file: file,
-        line: line)
-}
-
-public func assertNotEqual<T>(
-    _ expression1: @autoclosure () throws -> T,
-    _ expression2: @autoclosure () throws -> T,
-    _ message: @autoclosure () -> String = "",
-    file: StaticString = #file,
-    line: UInt = #line) where T : Equatable
-{
-    XCTAssertNotEqual(
-        try expression1(),
-        try expression2(),
-        message(),
-        file: file,
-        line: line)
-}
-
-public func assertNotEqual<T>(
-    _ expression1: @autoclosure () throws -> T,
-    _ expression2: @autoclosure () throws -> T,
-    accuracy: T,
-    _ message: @autoclosure () -> String = "",
-    file: StaticString = #file,
-    line: UInt = #line) where T : FloatingPoint
-{
-    XCTAssertNotEqual(
-        try expression1(),
-        try expression2(),
-        accuracy: accuracy,
-        message(),
-        file: file,
-        line: line)
-}
-
-public func assertThrowsError<T>(
-    _ expression: @autoclosure () throws -> T,
+public func expect<Error: Swift.Error, Result>(
+    throws error: Error,
     _ message: @autoclosure () -> String = "",
     file: StaticString = #file,
     line: UInt = #line,
-    _ errorHandler: (_ error: Error) -> Void = { _ in })
+    _ expression: () throws -> Result) where Error: Equatable
 {
+    func errorHandler(_error: Swift.Error) {
+        guard let __error = _error as? Error else {
+            fail(
+                "expected: \(Error.self), throwed: \(type(of: _error))",
+                file: file,
+                line: line)
+            return
+        }
+        expect(error == __error, message(), file: file, line: line)
+    }
+
     XCTAssertThrowsError(
         try expression(),
         message(),
         file: file,
         line: line,
         errorHandler)
-}
-
-public func assertNoThrow<T>(
-    _ expression: @autoclosure () throws -> T,
-    _ message: @autoclosure () -> String = "",
-    file: StaticString = #file,
-    line: UInt = #line)
-{
-    XCTAssertNoThrow(
-        try expression(),
-        message(),
-        file: file,
-        line: line)
 }
 
 public func fail(
--- Tests/TestTests/TestTests.swift
+++ Tests/TestTests/TestTests.swift
@@ -1,6 +1,30 @@
 @testable import Test
 
 class TestTests: TestCase {
+    func testExpect() {
+        expect(true == true)
+        expect(true != false)
+        expect(42 > 0)
+    }
+
+    func testExpectMessage() {
+        expect(true == true, "two true to be true")
+    }
+
+    func testExpectError() {
+        enum Error: Swift.Error {
+            case bsod
+        }
+
+        func runVisualStudio() throws {
+            throw Error.bsod
+        }
+
+        expect(throws: Error.bsod) {
+            try runVisualStudio()
+        }
+    }
+
     func testThrows() {
         func throwsError() throws {}
         scope {
@@ -27,6 +51,6 @@ class TestTests: TestCase {
                 called = true
             }
         }
-        assertTrue(called)
+        expect(called == true)
     }
 }
