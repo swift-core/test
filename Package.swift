424ab3a0de8483855988189d60335794943c6447
Tony Freeman <tonyfreeman@users.noreply.github.com>
2022-01-06 08:57:34 +0300

    [Package] Add trailing comma

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     products: [
         .library(
             name: "Test",
-            targets: ["Test"])
+            targets: ["Test"]),
     ],
     targets: [
         .target(
